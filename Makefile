9782b62603dc8c8310f1447e95f2bbb32148729f
Kevin Johnson <kevinstuartjohnson@gmail.com>
2018-09-03 16:56:16 -0400

    siso

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 ls:
 	gcc -o build/ls src/ls.c -I include/ -L lib/ -lapue -Wall
 
+siso:
+	gcc -o build/siso src/siso.c -I include/ -L lib/ -lapue -Wall
---
+++ src/siso.c
@@ -0,0 +1,17 @@
+#include "apue.h"
+
+#define BUFFSIZE 4096
+
+int main(void) {
+    int     n;
+    char    buf[BUFFSIZE];
+
+    while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
+        if (write(STDOUT_FILENO, buf, n) != n)
+            err_sys("Write Error");
+
+    if (n < 0)
+        err_sys("Read Error");
+
+    exit(0);
+}
